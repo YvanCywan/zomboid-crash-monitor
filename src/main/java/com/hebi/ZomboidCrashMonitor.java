56a98b7282039559052d189da1338ea5d5a713d4
YvanCywan <c1yvan@outlook.com>
2024-10-14 16:09:36 +0100

    Adding more logging

--- src/main/java/com/hebi/ZomboidCrashMonitor.java
+++ src/main/java/com/hebi/ZomboidCrashMonitor.java
@@ -44,7 +44,7 @@ public class ZomboidCrashMonitor {
 
         List<OSProcess> processes = os.getProcesses();
         boolean isRunning = processes.stream()
-                .anyMatch(osProcess -> osProcess.getName().contains(serviceName));
+                .anyMatch(osProcess -> osProcess.getName().contains("PZomboid6"));
 
         System.out.println("Processes: " + processes.stream().map(OSProcess::getName).toList());
 
